95fef39e55f979c242b0d5d7f4f000dcf41676d6
joeringer <joseph@achievr.io>
2019-09-02 20:56:35 +1000

    Implement WeekdaysBetweenTwoDates() and add unit tests.

--- BusDayCounterTests/BusDayCounterTests.cs
+++ BusDayCounterTests/BusDayCounterTests.cs
@@ -1,27 +1,43 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Tests
 {
     public class BusDayCounterTests
     {
+        List<DateTime> publicHolidays = new List<DateTime>()
+        {
+            new DateTime(2013, 12, 25),
+            new DateTime(2013, 12, 26),
+            new DateTime(2014, 1, 1)
+        };
+
         [SetUp]
         public void Setup()
         {
         }
 
-        /*
-         * SAMPLE TESTS:
-         * 
-            Start Date End Date Result
-            7th October 2013 9th October 2013 1
-            5th October 2013 14th October 2013 5
-            7th October 2013 1st January 2014 61
-            7th October 2013 5th October 2013 0
-        */
         [Test]
-        public void TestWeekdays()
+        public void TestWeekdaysEqual()
         {
+            // 7th October 2013 - 7th October 2013 should have 0 weekdays
+            Assert.AreEqual(0, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 7)));
+        }
+
+        [Test]
+        public void TestWeekdaysNegativeRange()
+        {
+            // 7th October 2013 - 5th October 2013 should have 0 weekdays
+            Assert.AreEqual(0, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5)));
+        }
+
+        [Test]
+        public void TestWeekdaysStandardRange()
+        {
+            // 25th October 2013 - 28th October 2013 should have 0 weekdays
+            Assert.AreEqual(0, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2019, 10, 25), new DateTime(2019, 10, 28)));
+
             // 7th October 2013 - 9th October 2013 should have 1 weekday
             Assert.AreEqual(1, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
 
@@ -30,10 +46,6 @@ namespace Tests
 
             // 7th October 2013 - 1st January 2014 should have 61 weekdays
             Assert.AreEqual(61, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
-
-            // 7th October 2013 - 5th October 2013 should have 0 weekdays
-            Assert.AreEqual(0, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5)));
-
         }
 
         /*
@@ -48,13 +60,13 @@ namespace Tests
         public void TestBusinessDays()
         {
             // 7th October 2013 - 9th October 2013 should have 1 business day
-            Assert.AreEqual(1, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)));
+            Assert.AreEqual(1, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), publicHolidays));
 
             // 24th December 2013 - 27th December 2013 should have 0 business days
-            Assert.AreEqual(0, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)));
+            Assert.AreEqual(0, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), publicHolidays));
 
             // 7th October 2013 - 1st January 2014 should have 59 business days
-            Assert.AreEqual(59, BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)));
+            Assert.AreEqual(59, BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), publicHolidays));
 
         }
 
--- BusinessDayCounter/BusinessDayCounter.cs
+++ BusinessDayCounter/BusinessDayCounter.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 
 public class BusinessDayCounter
 {
+
+    private static bool IsWeekday(int dateIndex)
+    {
+        return !(dateIndex == 0 || dateIndex == 6);
+    }
+    
     public static int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
     {
-        throw new NotImplementedException();
+        TimeSpan timeSpan = secondDate.Subtract(firstDate);
+
+        // Subtract 1 because looking at exclusive range between dates
+        int totalDays = Math.Max(0, (timeSpan.Days - 1));
+
+        // 5 days for each complete week plus remaining days
+        int totalWeekdays = ((totalDays / 7) * 5) + totalDays % 7;
+
+        if (totalDays > 0)
+        {
+            int firstDayIndex = ((int)firstDate.DayOfWeek + 1) % 6;
+            int lastDayIndex = (int)secondDate.DayOfWeek - 1;
+
+            // Remove a weekday if first day in range is Sunday or last day is Saturday
+            if (!IsWeekday(firstDayIndex))
+                totalWeekdays--;
+            if (!IsWeekday(lastDayIndex))
+                totalWeekdays--;
+        }
+
+        return totalWeekdays;
     }
+
     public static int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
     {
         throw new NotImplementedException();
